36e3d608a53f05433816de2c02a5f55f36128abd
Ivan Golubev <mail.igolubev@gmail.com>
2017-11-12 19:08:57 +0300

    less added

---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    </br>
+    <span>la la la</span>
+    <!--displays list -->
+    <ul>
+      <li v-for="item in listItems">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      listItems: ['buy food', 'play games', 'sleep']
+    }
+  }
+}
+</script>
+
+<style scoped="" lang="less">
+  div {
+    color: yellow;
+    li {
+      color: blue;
+    }
+  }
+</style>
---
+++ src/components/WebDesigner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    </br>
+    <span>
+      {{webdesigner}}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'webdesigner',
+  data () {
+    return {
+      webdesigner: 'this is web designer component'
+    }
+  }
+}
+</script>
+
+<style scoped="" lang="less">
+  div {
+    color: red;
+  }
+</style>
