62e396ea92432faa44e0bfab5782cfd40f0c01b7
Ivan Golubev <mail.igolubev@gmail.com>
2017-11-14 23:50:01 +0300

    add styles and cut redundance code

--- src/components/WebDesigner.vue
+++ src/components/WebDesigner.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
-    </br>
-    <span>
-      {{webdesigner}}
-    </span>
+  <div class='area'>
+  
   </div>
+  <div class='controls'>
+    <input class="btn btn-default" type="button" value="Add table">
+  </div>
+</div>
 </template>
 
 <script>
+// import axios from 'axios'
+
+// const API_BASE = 'http://websqldesignerserver'
+
 export default {
   name: 'webdesigner',
+  created () {
+    console.log('webdesigner component created')
+  //  axios.get(`${API_BASE}/backend/php-mysql/index.php`).then((response) => {
+  //    console.log('HTTP response!!!')
+  //    console.log(response)
+  //  })
+  },
   data () {
     return {
       webdesigner: 'this is web designer component'
@@ -18,8 +31,21 @@ export default {
 }
 </script>
 
-<style scoped="" lang="less">
-  div {
-    color: red;
+<style lang="less">
+  .area {
+    position: relative;
+    background: transparent url(../assets/back.png);
+    width: 3000px;
+    height: 3000px;
+  }
+
+  .controls {
+    text-align: center;
+    background-color: #ccc;
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    width: 150px;
+    padding: 0px 5px 5px;
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import Router from 'vue-router'
 import List from '@/components/List'
 import WebDesigner from '@/components/WebDesigner'
 
+//  import axios from 'axios'
+
 Vue.use(Router)
+
+//  const API_BASE = 'http://localhost:8080'
 
 export default new Router({
   routes: [
